474828c4a7a9e2c02bc4bae330f4b82d5c63250c
berşan <93613612+bersankaya@users.noreply.github.com>
2022-09-07 23:59:08 +0300

    SifreliVeriLer
    sqle Şifreli Halde Bilgileri Gönderme

---
+++ SifreliVeriler/SifreliVeriler/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿
+namespace SifreliVeriler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtAd = new System.Windows.Forms.TextBox();
+            this.TxtSoyad = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtMail = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtSifre = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TxtHesapNo = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.BtnKaydet = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(1, 204);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(919, 288);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(57, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 27);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ad:";
+            // 
+            // TxtAd
+            // 
+            this.TxtAd.Location = new System.Drawing.Point(104, 22);
+            this.TxtAd.Name = "TxtAd";
+            this.TxtAd.Size = new System.Drawing.Size(193, 34);
+            this.TxtAd.TabIndex = 2;
+            // 
+            // TxtSoyad
+            // 
+            this.TxtSoyad.Location = new System.Drawing.Point(104, 62);
+            this.TxtSoyad.Name = "TxtSoyad";
+            this.TxtSoyad.Size = new System.Drawing.Size(193, 34);
+            this.TxtSoyad.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(24, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 27);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Soyad:";
+            // 
+            // TxtMail
+            // 
+            this.TxtMail.Location = new System.Drawing.Point(104, 102);
+            this.TxtMail.Name = "TxtMail";
+            this.TxtMail.Size = new System.Drawing.Size(193, 34);
+            this.TxtMail.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(43, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 27);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Mail:";
+            // 
+            // TxtSifre
+            // 
+            this.TxtSifre.Location = new System.Drawing.Point(469, 22);
+            this.TxtSifre.Name = "TxtSifre";
+            this.TxtSifre.Size = new System.Drawing.Size(193, 34);
+            this.TxtSifre.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(403, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 27);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Şifre:";
+            // 
+            // TxtHesapNo
+            // 
+            this.TxtHesapNo.Location = new System.Drawing.Point(469, 65);
+            this.TxtHesapNo.Name = "TxtHesapNo";
+            this.TxtHesapNo.Size = new System.Drawing.Size(193, 34);
+            this.TxtHesapNo.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(352, 62);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(111, 27);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Hesap No:";
+            // 
+            // BtnKaydet
+            // 
+            this.BtnKaydet.Location = new System.Drawing.Point(508, 105);
+            this.BtnKaydet.Name = "BtnKaydet";
+            this.BtnKaydet.Size = new System.Drawing.Size(119, 34);
+            this.BtnKaydet.TabIndex = 11;
+            this.BtnKaydet.Text = "Kaydet";
+            this.BtnKaydet.UseVisualStyleBackColor = true;
+            this.BtnKaydet.Click += new System.EventHandler(this.BtnKaydet_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(790, 164);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 34);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "çöz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 27F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Maroon;
+            this.ClientSize = new System.Drawing.Size(921, 491);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.BtnKaydet);
+            this.Controls.Add(this.TxtHesapNo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.TxtSifre);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TxtMail);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TxtSoyad);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtAd);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtAd;
+        private System.Windows.Forms.TextBox TxtSoyad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtMail;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtSifre;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TxtHesapNo;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button BtnKaydet;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
